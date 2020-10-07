577de9193ad5bf1f483286b3e2fd5134389254c3
Chandrika <sejutispr.dey01@gmail.com>
2020-10-06 22:07:02 -0400

    string hw

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        char[] hiArray = { 'h' , 'i' , '.' };
+        String hiString = new String(hiArray);
+        System.out.println( hiString );
+
+
+    }
+}
